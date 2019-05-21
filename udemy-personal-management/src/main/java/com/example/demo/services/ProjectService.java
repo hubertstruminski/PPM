64614f680593c3616111b9d090684e68eddea316
hubert.struminski <hubert.struminski@gmail.com>
2019-05-21 22:07:04 +0200

    add method for save to Project service and add request mapping for post in Project controller class

---
+++ udemy-personal-management/src/main/java/com/example/demo/services/ProjectService.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+import com.example.demo.domain.Project;
+import com.example.demo.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
