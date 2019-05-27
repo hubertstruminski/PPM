beb142905359b1c54dac08c71615eabc3ee450b7
hubert.struminski <hubert.struminski@gmail.com>
2019-05-27 18:43:44 +0200

    fix bug

--- ppm-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppm-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -38,7 +38,13 @@ class ProjectBoard extends React.Component {
                             {errors.projectNotFound}
                         </div>
                     );
-                } else {
+                } else if (errors.projectIdentifier) {
+                    return (
+                        <div className="alert alert-danger text-center" role="alert">
+                            {errors.projectIdentifier}
+                        </div>
+                    );
+                }else {
                     return (
                         <div className="alert alert-info text-center" role="alert">
                             No Project Tasks on this board
